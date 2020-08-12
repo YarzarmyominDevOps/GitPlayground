24bd0d64c0262787d36bb246459de9a254c822a5
Yarzar Myo Min <yarzarmyomin1997@gmail.com>
2020-08-12 23:57:45 +0630

    Add file to Dev Branch

---
+++ Test/Views/DevelopPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Test.Views
+{
+    public partial class DevelopPage : ContentPage
+    {
+        string a = "";
+        public DevelopPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
