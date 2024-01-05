fb30776c39ab2c411a3393dd1e21bbbdb4e9f8c3
Yarzarmyomin97 <yarzarmyomin1997@gmail.com>
2024-01-05 14:42:23 +0630

    add operation.js

---
+++ operations.js
@@ -0,0 +1,19 @@
+// Add two numbers
+const add = (a, b) => {
+  // TODO: Implement here
+  return 0;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+// Multiply two numbers
+const mulltiply = (a, b) => {
+  return a * b;
+};
+
+// Divide two numbers
+const divide = (a, b) => {
+  return a / b;
+};
